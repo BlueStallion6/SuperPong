a36ff3b67976ded7c67a2b1b14faa1471a4e7162
Reclipse <narcisprecup233@gmail.com>
2024-04-25 14:59:44 +0300

    True initial commit

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-print('ma ta')
-print('sugus')
-print('ahuuu')
+#Pune importurile aici
+#
+#Dupa ce instalezi importurile baga in consola "pip freeze > requirements.txt
+try:
+    import pygame
+except ImportError:
+    print("Please run 'pip install -r requirements.txt in this folder's directory.")
 
-print('chestie')
+
+#Pune codu aci
+def main():
+    print("It works!")
+
+
+if __name__ == "__main__":
+    main()
