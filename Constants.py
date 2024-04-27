f1653e883268d532f59b3b33d058d489905a57a3
Denis <denissas2014@gmail.com>
2024-04-27 23:55:49 +0300

    progress #1.7

---
+++ Constants.py
@@ -0,0 +1,35 @@
+import json
+import pygame
+from keywords import *
+from screeninfo import get_monitors
+
+
+try:
+    open("config.json")
+except FileNotFoundError:
+    print_error("Config file not found.")
+
+with open("config.json", "r") as file:
+    CONFIG = json.load(file)
+    # print_debug(str(config["settings"]))
+    file.close()
+
+if CONFIG["settings"]["window_perc"] is None:
+    #FALLBACK VALUE
+    W_PERC = 0.5  
+else:
+    W_PERC = CONFIG["settings"]["window_perc"] / 100
+
+MONITORS = get_monitors()
+primary_monitor = MONITORS[0]
+
+DEBUG_MODE = CONFIG["settings"]["debug_mode"]
+W_WIDTH = primary_monitor.width * W_PERC
+W_HEIGHT = primary_monitor.height * W_PERC
+PADDLE_WIDTH, PADDLE_HEIGHT = CONFIG["play_configs"]["paddle_width"] * W_PERC, CONFIG["play_configs"]["paddle_height"] * W_PERC
+TPS = CONFIG["settings"]["tps"]
+PADDLE_SPEED = CONFIG["play_configs"]["paddle_speed"] * W_PERC
+RP_MULT = 1
+LP_MULT = 1
+
+FLAGS = pygame.HWSURFACE | pygame.DOUBLEBUF
--- main.py
+++ main.py
@@ -1,64 +1,37 @@
-#Dupa ce instalezi importurile baga in consola "pip freeze > requirements.txt"
+#~MAIN FILE START
+
 try:
     import os
     import pygame
     import colored
-    from keywords import *
     import sys
-    import resources.pygameResources as assets
     import screeninfo
     import json
     from screeninfo import get_monitors
+
+    import resources.pygameResources as assets
+    from keywords import *
+    from Constants import *
 except ImportError:
     print("ImportError >> Please run 'pip install -r requirements.txt' in this project's directory.")
     exit()
 
+##########################################################################################################################################
+#~WINDOW INIT~
 pygame.init()
-
-try:
-    open("config.json")
-except FileNotFoundError:
-    print_error("Config file not found, creating one...")
-    with open("config.json", "a") as file:
-        json.dump('{"settings":{"window_perc": null,"debug_mode": 1}}', file, indent=4)
-        file.close()
-
-
 pygame.display.set_caption('SuperPong')
 clock = pygame.time.Clock()
+screen = pygame.display.set_mode((W_WIDTH, W_HEIGHT), FLAGS)
+##########################################################################################################################################
 
-monitors = get_monitors()
-primary_monitor = monitors[0]
-
-with open("config.json", "r") as file:
-    config = json.load(file)
-    # print_debug(str(config["settings"]))
-    file.close()
-
-if config["settings"]["window_perc"] is None:
-    w_perc = int(input(f"{c_white}~Please specify the window size (1-100): {c_rst}"))
+if CONFIG["settings"]["window_perc"] is None:
+    W_PERC = int(input(f"{c_white}~Please specify the window size (1-100): {c_rst}"))
     if input(f"{c_white}~Do you want to save your selection for the next time? (y/n): ").lower() == "y":
-        config["settings"]["window_perc"] = w_perc
+        CONFIG["settings"]["window_perc"] = W_PERC
         with open("config.json", "w") as file:
             # print_debug(str(config))
-            json.dump(config, file, indent=4)
+            json.dump(CONFIG, file, indent=4)
             file.close()
-else:
-    w_perc = config["settings"]["window_perc"] / 100
-
-flags = pygame.HWSURFACE | pygame.DOUBLEBUF
-
-DEBUG_MODE = config["settings"]["debug_mode"]
-W_WIDTH = primary_monitor.width * w_perc
-W_HEIGHT = primary_monitor.height * w_perc
-PADDLE_WIDTH, PADDLE_HEIGHT = config["play_configs"]["paddle_width"] * w_perc, config["play_configs"]["paddle_height"] * w_perc
-TPS = config["settings"]["tps"]
-PADDLE_SPEED = config["play_configs"]["paddle_speed"] * w_perc
-
-RP_MULT = 1
-LP_MULT = 1
-
-screen = pygame.display.set_mode((W_WIDTH, W_HEIGHT), flags)
 
 
 class Paddle:
@@ -71,20 +44,18 @@ class Paddle:
     def draw(self, win):
         pygame.draw.rect(win, Colors.LIGHT_GRAY, (self.x, self.y, self.width, self.height))
 
+left_paddle = Paddle(20, W_HEIGHT/2 - PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT * LP_MULT)
+right_paddle = Paddle(W_WIDTH - 20 - PADDLE_WIDTH, W_HEIGHT/2 - PADDLE_HEIGHT/ 2, PADDLE_WIDTH, PADDLE_HEIGHT * RP_MULT)
+
 
 class Ball:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.radius = config["play_configs"]["ball_radius"]
+        self.radius = CONFIG["play_configs"]["ball_radius"]
 
     def draw(self, win):
         pygame.draw.circle(win, Colors.WHITE, (self.x, self.y, self.radius))
-
-
-left_paddle = Paddle(20, W_HEIGHT/2 - PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT * LP_MULT)
-right_paddle = Paddle(W_WIDTH - 20 - PADDLE_WIDTH, W_HEIGHT/2 - PADDLE_HEIGHT/ 2, PADDLE_WIDTH, PADDLE_HEIGHT * RP_MULT)
-
 
 
 running = True
