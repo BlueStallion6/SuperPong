66ae4ef7a52b1f729a8780ecff6d9d6fc66ef6f9
Denis <denissas2014@gmail.com>
2024-04-30 18:29:15 +0300

    #2.4.1

--- resources/pygameResources.py
+++ resources/pygameResources.py
@@ -6,3 +6,7 @@ sfx = pygame.mixer.Sound
 MARGIN_HIT_SOUND = sfx(".\\resources\\sounds\\pong_sound2.wav")
 PADDLE_HIT_SOUND = sfx(".\\resources\\sounds\\pong_sound1.wav")
 WIN_LOSE_SOUND = sfx(".\\resources\\sounds\\pickupCoin2.wav")
+
+MARGIN_HIT_SOUND.set_volume(0.4)
+PADDLE_HIT_SOUND.set_volume(0.45)
+WIN_LOSE_SOUND.set_volume(0.32)
